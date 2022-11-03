a7dba54f055d728faa3a39e8c1b10878bffad86e
Ali <abhatti89@gmail.com>
2022-11-03 12:53:14 +0000

    URL object added to mongodb

--- api/models/post.js
+++ api/models/post.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
   message: String,
-  date: { type: Date, default: Date.now }
+  date: { type: Date, default: Date.now },
+    url: {
+      work: mongoose.SchemaTypes.Url,
+      profile: mongoose.SchemaTypes.Url
+    }
 });
 
 const Post = mongoose.model("Post", PostSchema);
