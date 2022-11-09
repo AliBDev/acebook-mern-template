7d01dafb00a81200835247cd7b93690cd6399aa8
Ali <abhatti89@gmail.com>
2022-11-09 17:41:15 +0000

    Trying out different borders on individual posts

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -3,25 +3,24 @@ import './Post.css'
 
 const Post = ({post}) => {
   return (
-    <article data-cy="post" key={post._id}>
-
-      <div className="profile-picture-container">
-        <img src="https://firebasestorage.googleapis.com/v0/b/acebook-pic-files.appspot.com/o/images%2Fprofilepic%2Fdefaultprofilepics%2FWG.jpeg?alt=media&token=7de0a933-e9af-4921-8496-1e76ff23515a" alt='' />
-      </div>
-
-      <div class="message-container">
-        <div class="message">{post.message}</div>
-      </div>
-      
-      <div class="post-image-container">
-        <img class="post-image" src={post.img} alt='' />
-      </div>
-
-      <div class="header-container">
-        <div class="name-and-time-container">
-          <div class="timestamp">{post.date}</div>
+    <article class="single-post-container" data-cy="post" key={post._id}>
+        <div className="profile-picture-container">
+          <img src="https://firebasestorage.googleapis.com/v0/b/acebook-pic-files.appspot.com/o/images%2Fprofilepic%2Fdefaultprofilepics%2FWG.jpeg?alt=media&token=7de0a933-e9af-4921-8496-1e76ff23515a" alt='' />
         </div>
-      </div>
+
+        <div class="message-container">
+          <div class="message">{post.message}</div>
+        </div>
+        
+        <div class="post-image-container">
+          <img class="post-image" src={post.img} alt='' />
+        </div>
+
+        <div class="header-container">
+          <div class="name-and-time-container">
+            <div class="timestamp">{post.date}</div>
+          </div>
+        </div>
     </article>
   )
 }
